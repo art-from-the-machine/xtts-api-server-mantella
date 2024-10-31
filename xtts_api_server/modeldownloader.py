386b74e73bad799d7478b00eff8d6d63f3d908ab
Art from the Machine <artfromthemachine@outlook.com>
2024-10-31 10:01:45 +0100

    Set explicit deepspeed version for Linux

--- xtts_api_server/modeldownloader.py
+++ xtts_api_server/modeldownloader.py
@@ -62,9 +62,10 @@ def install_deepspeed_based_on_python_version():
 
             else:
                 logger.error("Unsupported Python version on Windows.")
+                deepspeed_link = 'deepspeed==0.15.2'
 
         else: # Assuming Linux/MacOS otherwise (add specific checks if necessary)
-             deepspeed_link = 'deepspeed'
+             deepspeed_link = 'deepspeed==0.15.2'
 
         if deepspeed_link:
              logger.info("Installing DeepSpeed...")
